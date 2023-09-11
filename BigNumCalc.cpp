0fa80e466f8e3545a5bbdbf43f6aaa9b07d5c4a2
hpcatley <hpcatley@gmail.com>
2023-09-11 13:58:26 +0930

    attempt 5

---
+++ BigNumCalc.cpp
@@ -0,0 +1,22 @@
+#include "BigNumCalc.h"
+#include "LinkedList.h"
+#include "Node.h"
+#include <iostream>
+#include <list>
+#include <string>
+
+std::list<int> buildBigNum(std::string numString) {
+
+}
+
+std::list<int> add(std::list<int> num1, std::list<int> num2) {
+
+}
+
+std::list<int> sub(std::list<int> num1, std::list<int> num2) {
+
+}
+
+std::list<int> mul(std::list<int> num1, std::list<int> num2) {
+    
+}
---
+++ BigNumCalc.h
@@ -0,0 +1,21 @@
+#ifndef BIGNUMCALC_H
+#define BIGNUMCALC_H
+#include <string>
+#include <iostream>
+#include <list>
+
+class bigNumCalc {
+    protected:
+        int num1;
+        int num2;
+
+    public:
+        std::list<int> buildBigNum(std::string numString);
+        std::list<int> add(std::list<int> num1, std::list<int> num2);
+        std::list<int> sub(std::list<int> num1, std::list<int> num2);
+        std::list<int> mul(std::list<int> num1, std::list<int> num2);
+};
+
+
+
+#endif BIGNUMCALC_H
